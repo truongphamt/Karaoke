bdda86cbebd361d7b729ba817f9feb6149eaa028
Truong Pham <truong.pham@flowcal.com>
2019-02-25 23:00:40 -0600

    Initial Commit

---
+++ Karaoke/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Karaoke
+//
+//  Created by Anh Phung on 2/25/19.
+//  Copyright © 2019 TPham. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
